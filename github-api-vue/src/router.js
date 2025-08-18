2cd2d8faeb05a0fa9bd1ef7785355b9d9ff2970b
Hanjoong.Cho <hanjoongcho@gmail.com>
2025-08-18 22:40:51 +0900

    fix: refactoring

--- github-api-vue/src/router.js
+++ github-api-vue/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import GithubRepoTree from './components/GithubRepoTree.vue';
 import Sample1 from './components/Sample1.vue';
 import Sample2 from './components/Sample2.vue';
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/copilot-demo/'), // base 경로 추가
+  history: createWebHashHistory('/copilot-demo/'), // base 경로 추가
   routes,
 });
 
